1ab411abf353c4d962a2e93936ce6339c31258d2
Safin-Momin <m.safin9466@gmail.com>
2023-08-04 12:56:48 +0530

    Add files

---
+++ dist/script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  let result = eval(display.value);
+  display.value = result;
+}
+
+function clearDisplay() {
+  // Clear the calculator display
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  const displayValue = document.getElementById("display").value;
+  let result;
+
+  try {
+    result = eval(displayValue);
+  } catch (error) {
+    result = "Error";
+  }
+
+  document.getElementById("display").value = result;
+}
