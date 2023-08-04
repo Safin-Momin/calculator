bf39426e850fd8877834239498a18a8c1daff9a2
Safin-Momin <m.safin9466@gmail.com>
2023-08-04 14:45:39 +0530

    modified js file

--- dist/script.js
+++ dist/script.js
@@ -12,21 +12,3 @@ function calculate() {
   let result = eval(display.value);
   display.value = result;
 }
-
-function clearDisplay() {
-  // Clear the calculator display
-  document.getElementById("display").value = "";
-}
-
-function calculate() {
-  const displayValue = document.getElementById("display").value;
-  let result;
-
-  try {
-      result = eval(displayValue);
-  } catch (error) {
-    result = "Error";
-  }
-
-    document.getElementById("display").value = result;
-}
