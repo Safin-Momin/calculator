bf0e665f00dc2f75bd33f87c18abdb0fef1a314a
Safin-Momin <m.safin9466@gmail.com>
2023-08-04 17:55:27 +0530

    After useful changes

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,28 @@
 let display = document.getElementById("display");
+let isCalculationDone = false;
 
 function appendValue(value) {
+  if (isCalculationDone && isNaN(value)) {
+    isCalculationDone = false;
+  }
+  if (isCalculationDone && !isNaN(value)) {
+    clearDisplay();
+    isCalculationDone = false;
+  }
   display.value += value;
 }
 
 function clearDisplay() {
   display.value = "";
+  isCalculationDone = false;
 }
 
 function calculate() {
   let result = eval(display.value);
   display.value = result;
+  isCalculationDone = true; 
+}
+
+function clearLastDigit() {
+  display.value = display.value.slice(0, -1);
 }
